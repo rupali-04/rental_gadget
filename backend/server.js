cf9a6e34eb43e3a7c5b76d69aab976d261c14852
rupali-04 <rupalishah040@gmail.com>
2023-10-01 23:22:54 +0530

    update port for localhost

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Initiallizing the app port 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT,()=>{
     console.log(`Server is Running at PORT ${PORT}`)
