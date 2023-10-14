6023ab4908216e73423abcd17d43d352c00f9318
rupali-04 <rupalishah040@gmail.com>
2023-10-15 01:30:38 +0530

    frontend

--- frontend/src/app/renter/renter.component.ts
+++ frontend/src/app/renter/renter.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-renter',
   templateUrl: './renter.component.html',
-  styleUrls: ['./renter.component.css']
+  styleUrls: ['./renter.component.css'],
 })
-export class RenterComponent {
+export class RenterComponent implements OnInit {
+  form: any = {
+    title: null,
+    description: null,
+    available: null,
+    photoGalary: null,
+    gadgetType: null,
+    brand: null,
+    gadgetModel: null,
+    specification: null,
+    serialNumber: null,
+    fromAvailableDate: null,
+    toAvailableDate: null,
+    securityDeposit: null,
+    rentalRate: null,
+    gadgetLocation: null,
+    userDetails: null,
+    discountCoupon: null,
+    date: Date.now(),
+  };
 
+  errorMessage: string = '';
+  roles: string[] = [];
+
+  constructor(
+    private authService: AuthService,
+    private storageServie: StorageService
+  ) {}
+
+  ngOnInit(): void {
+    console.log(this.storageServie.getUser());
+    const userRoles = this.storageServie.getUser().roles;
+    if (userRoles.includes('Renter')) {
+    }
+  }
 }
